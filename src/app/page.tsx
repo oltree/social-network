77efcc377c982798f35e27ba56cce8ddcbd76e23
oltree <oleg.melekh.frontend@gmail.com>
2023-12-17 17:44:17 +0300

    start chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import ChatsList from '@/components/screens/chats/chat-list/ChatList';
+import Chat from '@/components/screens/chats/chat/Chat';
+import CurrentUser from '@/components/screens/chats/current-user/CurrentUser';
+
+export default function ChatsPage() {
+  return (
+    <div
+      className='grid h-full'
+      style={{
+        gridTemplateColumns: '.7fr 3fr',
+      }}
+    >
+      <div className='border-r border-border'>
+        <CurrentUser />
+        <ChatsList />
+      </div>
+
+      <div>
+        <Chat />
+      </div>
+    </div>
+  );
 }
